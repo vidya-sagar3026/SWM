72cb32908e0cde0fa49f978f50c916c688f8e6af
vidya-sagar3026 <42702283+vidya-sagar3026@users.noreply.github.com>
2021-11-26 14:02:34 +0530

    testapicall update

--- testapicall.js
+++ testapicall.js
@@ -3,28 +3,28 @@ var tagId = 'GHZBDCB000001';
 var query = 'http://'+ip+':8081/api/swm/report/nodeIdForQrCode/?qrCode='+tagId;
 var fetchedJson;
 const axios = require('axios');
-     axios.get(query).then( resp => {
-      console.log(resp.data);
-      fetchedJson = resp.data;
-      console.log("fetched_json_in_apicalling",fetchedJson);
+    //  axios.get(query).then( resp => {
+    //   console.log(resp.data);
+    //   fetchedJson = resp.data;
+    //   console.log("fetched_json_in_apicalling",fetchedJson);
       
       
-    });
-var arr = [{"a":1,"b":3}];
-for(let i=0;i<arr.length;i++)
-{
-  console.log(arr[i].a);
-}
-var a = [];
-a[3] = 2;
-if(a[1]==undefined)
-{
-  console.log("key 1 does not exist");
-}
-if(a[2]==undefined)
-{
-  console.log("key 2 does not exist");
-}
+    // });
+// var arr = [{"a":1,"b":3}];
+// for(let i=0;i<arr.length;i++)
+// {
+//   console.log(arr[i].a);
+// }
+// var a = [];
+// a[3] = 2;
+// if(a[1]==undefined)
+// {
+//   console.log("key 1 does not exist");
+// }
+// if(a[2]==undefined)
+// {
+//   console.log("key 2 does not exist");
+// }
 
   //const axios = require('axios');
   if(false)
